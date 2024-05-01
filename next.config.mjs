a17342e2d50b2cacb2fc844143d85a6b586ebffd
MatheusThiesen <matheusreis314@gmail.com>
2024-05-01 11:30:10 -0300

    adicionando opcao de sctricmode

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: [
       "alpar2.sfo3.digitaloceanspaces.com",
       "alpar.sfo3.digitaloceanspaces.com",
-      "digitaloceanspaces.com",
-      "alpardobrasil.com.br",
       "dashboard.alpardobrasil.com.br",
       "https://dashboard.alpardobrasil.com.br",
     ],
