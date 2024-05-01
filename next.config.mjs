a65b86f7d6d02f6fed7c6bc746ff3dabf5bc736d
MatheusThiesen <matheusreis314@gmail.com>
2024-04-30 22:54:56 -0300

    adicionando dominio da imagem

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["alpar2.sfo3.digitaloceanspaces.com"],
+    domains: [
+      "alpar2.sfo3.digitaloceanspaces.com",
+      "alpar.sfo3.digitaloceanspaces.com",
+      "digitaloceanspaces.com",
+    ],
   },
 };
 
